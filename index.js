53f7cf1b1c6783f56df1b090e290f16f723d9be8
teng <1474218225@qq.com>
2018-06-14 10:25:34 +0800

    腾腾修改了

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var oll=document.getElementsByName("li");
  for(var i in oll){
      oll.index=i;
      oll[i].onclick=function(){
-         console.log(433)
+         /* console.log(433) */
+         for(var j in oll){
+             oll[j].className="";
+         }
+         all[this.index].className="active";
      }
  }
